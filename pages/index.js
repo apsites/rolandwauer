76488b6e000a15c0ca6d71347f4788f572e321e9
apsites <alejandro@authorspress.com>
2020-08-06 16:35:18 -0700

    fix image on mobile

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,9 @@ const Navbar = dynamic(() => import("../components/Navbar"));
 const Book = dynamic(() => import("../components/Book"));
 const Author = dynamic(() => import("../components/Author"));
 const Contact = dynamic(() => import("../components/Contact"));
-const DesktopNavbar = dynamic(() => import("../components/DesktopNavbar"))
+const DesktopNavbar = dynamic(() => import("../components/DesktopNavbar"));
+const Gallery = dynamic(() => import("../components/Gallery"));
+
 export default class Home extends Component {
   constructor(props) {
     super(props);
